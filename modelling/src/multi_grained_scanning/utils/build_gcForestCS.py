6aa5e9f29aa20e4cccffe020d9244c4a6074c312
liamcarew <liam.carew1@gmail.com>
2022-08-10 18:16:25 +0200

    update to gcForestCS config function

--- modelling/src/multi_grained_scanning/utils/build_gcForestCS.py
+++ modelling/src/multi_grained_scanning/utils/build_gcForestCS.py
@@ -75,19 +75,69 @@ def build_gcforestCS(n_estimators_mgs, tree_diversity_mgs, n_estimators_ca, tree
     net = {}
 
     net["outputs"] = []
-    # net["outputs"].append("win1/4x4/ets")
-    # net["outputs"].append("win1/4x4/rf")
+    # net["outputs"].append("win1/3x3/ets")
+    net["outputs"].append("pool/3x3/rf")
+    #net["outputs"].append("pool/4x4/ets")
+    net["outputs"].append("pool/4x4/rf")
     #net["outputs"].append("pool/5x5/ets")
     net["outputs"].append("pool/5x5/rf")
-    #net["outputs"].append("pool/7x7/ets")
-    net["outputs"].append("pool/7x7/rf")
     #net["outputs"].append("pool/9x9/ets")
-    net["outputs"].append("pool/9x9/rf")
+    #net["outputs"].append("pool/9x9/rf")
 
     if tree_diversity_mgs:
+      net["outputs"].append("pool/3x3/ets")
+      net["outputs"].append("pool/4x4/ets")
       net["outputs"].append("pool/5x5/ets")
-      net["outputs"].append("pool/7x7/ets")
-      net["outputs"].append("pool/9x9/ets")
+
+    #3x3 sliding window
+    layer_3x3 = {}
+    layer_3x3["type"] = "FGWinLayer"
+    layer_3x3["name"] = "win/3x3"
+    layer_3x3["bottoms"] = []
+    layer_3x3["bottoms"].append("X")
+    layer_3x3["bottoms"].append("y")
+    layer_3x3["tops"] = []
+    #layer_3x3["tops"].append("win/3x3/ets")
+    layer_3x3["tops"].append("win/3x3/rf")
+    layer_3x3["n_classes"] = 5
+    layer_3x3["estimators"] = []
+    layer_3x3["estimators"].append(
+        {"n_folds":5,"type":"RandomForestClassifier","n_estimators": n_estimators_mgs,"max_depth": 10,"n_jobs":40,"min_samples_leaf":10})
+    
+    if tree_diversity_mgs:
+      layer_3x3["tops"].append("win/3x3/ets")
+      layer_3x3["estimators"].append(
+          {"n_folds":5,"type":"ExtraTreesClassifier","n_estimators": n_estimators_mgs,"max_depth": 10, "n_jobs":40, "min_samples_leaf":10})
+
+    layer_3x3["stride_x"] = 2
+    layer_3x3["stride_y"] = 2
+    layer_3x3["win_x"] = 3
+    layer_3x3["win_y"] = 3
+
+    #4x4 sliding window
+    layer_4x4 = {}
+    layer_4x4["type"] = "FGWinLayer"
+    layer_4x4["name"] = "win/4x4"
+    layer_4x4["bottoms"] = []
+    layer_4x4["bottoms"].append("X")
+    layer_4x4["bottoms"].append("y")
+    layer_4x4["tops"] = []
+    #layer_4x4["tops"].append("win/4x4/ets")
+    layer_4x4["tops"].append("win/4x4/rf")
+    layer_4x4["n_classes"] = 5
+    layer_4x4["estimators"] = []
+    layer_4x4["estimators"].append(
+        {"n_folds":5,"type":"RandomForestClassifier","n_estimators": n_estimators_mgs,"max_depth": 10,"n_jobs":40,"min_samples_leaf":10})
+    
+    if tree_diversity_mgs:
+      layer_4x4["tops"].append("win/4x4/ets")
+      layer_4x4["estimators"].append(
+          {"n_folds":5,"type":"ExtraTreesClassifier","n_estimators": n_estimators_mgs,"max_depth": 10, "n_jobs":40, "min_samples_leaf":10})
+
+    layer_4x4["stride_x"] = 2
+    layer_4x4["stride_y"] = 2
+    layer_4x4["win_x"] = 4
+    layer_4x4["win_y"] = 4
 
     #5x5 sliding window
     layer_5x5 = {}
@@ -115,93 +165,97 @@ def build_gcforestCS(n_estimators_mgs, tree_diversity_mgs, n_estimators_ca, tree
     layer_5x5["win_y"] = 5
 
     #7x7 sliding window
-    layer_7x7 = {}
-    layer_7x7["type"] = "FGWinLayer"
-    layer_7x7["name"] = "win/7x7"
-    layer_7x7["bottoms"] = []
-    layer_7x7["bottoms"].append("X")
-    layer_7x7["bottoms"].append("y")
-    layer_7x7["tops"] = []
-    #layer_7x7["tops"].append("win/7x7/ets")
-    layer_7x7["tops"].append("win/7x7/rf")
-    layer_7x7["n_classes"] = 5
-    layer_7x7["estimators"] = []
-    layer_7x7["estimators"].append(
-        {"n_folds":5,"type":"RandomForestClassifier","n_estimators": n_estimators_mgs,"max_depth": 10,"n_jobs":40,"min_samples_leaf":10})
+    #layer_7x7 = {}
+    #layer_7x7["type"] = "FGWinLayer"
+    #layer_7x7["name"] = "win/7x7"
+    #layer_7x7["bottoms"] = []
+    #layer_7x7["bottoms"].append("X")
+    #layer_7x7["bottoms"].append("y")
+    #layer_7x7["tops"] = []
+    ##layer_7x7["tops"].append("win/7x7/ets")
+    #layer_7x7["tops"].append("win/7x7/rf")
+    #layer_7x7["n_classes"] = 5
+    #layer_7x7["estimators"] = []
+    #layer_7x7["estimators"].append(
+    #    {"n_folds":5,"type":"RandomForestClassifier","n_estimators": n_estimators_mgs,"max_depth": 10,"n_jobs":40,"min_samples_leaf":10})
     
-    if tree_diversity_mgs:
-      layer_7x7["tops"].append("win/7x7/ets")
-      layer_7x7["estimators"].append(
-          {"n_folds":5,"type":"ExtraTreesClassifier","n_estimators": n_estimators_mgs,"max_depth": 10, "n_jobs":40, "min_samples_leaf":10})
+    #if tree_diversity_mgs:
+    #  layer_7x7["tops"].append("win/7x7/ets")
+    #  layer_7x7["estimators"].append(
+    #      {"n_folds":5,"type":"ExtraTreesClassifier","n_estimators": n_estimators_mgs,"max_depth": 10, "n_jobs":40, "min_samples_leaf":10})
 
-    layer_7x7["stride_x"] = 2
-    layer_7x7["stride_y"] = 2
-    layer_7x7["win_x"] = 7
-    layer_7x7["win_y"] = 7
+    #layer_7x7["stride_x"] = 2
+    #layer_7x7["stride_y"] = 2
+    #layer_7x7["win_x"] = 7
+    #layer_7x7["win_y"] = 7
 
     #9x9 sliding window
-    layer_9x9 = {}
-    layer_9x9["type"] = "FGWinLayer"
-    layer_9x9["name"] = "win/9x9"
-    layer_9x9["bottoms"] = []
-    layer_9x9["bottoms"].append("X")
-    layer_9x9["bottoms"].append("y")
-    layer_9x9["tops"] = []
+    #layer_9x9 = {}
+    #layer_9x9["type"] = "FGWinLayer"
+    #layer_9x9["name"] = "win/9x9"
+    #layer_9x9["bottoms"] = []
+    #layer_9x9["bottoms"].append("X")
+    #layer_9x9["bottoms"].append("y")
+    #layer_9x9["tops"] = []
     #layer_9x9["tops"].append("win/9x9/ets")
-    layer_9x9["tops"].append("win/9x9/rf")
-    layer_9x9["n_classes"] = 5
-    layer_9x9["estimators"] = []
-    layer_9x9["estimators"].append(
-        {"n_folds":5,"type":"RandomForestClassifier","n_estimators": n_estimators_mgs,"max_depth": 10,"n_jobs":40,"min_samples_leaf":10})
+    #layer_9x9["tops"].append("win/9x9/rf")
+    #layer_9x9["n_classes"] = 5
+    #layer_9x9["estimators"] = []
+    #layer_9x9["estimators"].append(
+    #    {"n_folds":5,"type":"RandomForestClassifier","n_estimators": n_estimators_mgs,"max_depth": 10,"n_jobs":40,"min_samples_leaf":10})
     
-    if tree_diversity_mgs:
-      layer_9x9["tops"].append("win/9x9/ets")
-      layer_9x9["estimators"].append(
-          {"n_folds":5,"type":"ExtraTreesClassifier","n_estimators": n_estimators_mgs,"max_depth": 10, "n_jobs":40, "min_samples_leaf":10})
+    #if tree_diversity_mgs:
+    #  layer_9x9["tops"].append("win/9x9/ets")
+    #  layer_9x9["estimators"].append(
+    #      {"n_folds":5,"type":"ExtraTreesClassifier","n_estimators": n_estimators_mgs,"max_depth": 10, "n_jobs":40, "min_samples_leaf":10})
 
-    layer_9x9["stride_x"] = 2
-    layer_9x9["stride_y"] = 2
-    layer_9x9["win_x"] = 9
-    layer_9x9["win_y"] = 9
+    #layer_9x9["stride_x"] = 2
+    #layer_9x9["stride_y"] = 2
+    #layer_9x9["win_x"] = 9
+    #layer_9x9["win_y"] = 9
 
     #pooling layer
     layers_pool = {}
     layers_pool["type"] = "FGPoolLayer"
     layers_pool["name"] = "pool"
     layers_pool["bottoms"] = []
+    #layers_pool["bottoms"].append("win/3x3/ets")
+    layers_pool["bottoms"].append("win/3x3/rf")
+    #layers_pool["bottoms"].append("win/4x4/ets")
+    layers_pool["bottoms"].append("win/4x4/rf")
     #layers_pool["bottoms"].append("win/5x5/ets")
     layers_pool["bottoms"].append("win/5x5/rf")
     #layers_pool["bottoms"].append("win/7x7/ets")
-    layers_pool["bottoms"].append("win/7x7/rf")
+    #layers_pool["bottoms"].append("win/7x7/rf")
     #layers_pool["bottoms"].append("win/9x9/ets")
-    layers_pool["bottoms"].append("win/9x9/rf")
+    #layers_pool["bottoms"].append("win/9x9/rf")
 
     if tree_diversity_mgs:
+      layers_pool["bottoms"].append("win/3x3/ets")
+      layers_pool["bottoms"].append("win/4x4/ets")
       layers_pool["bottoms"].append("win/5x5/ets")
-      layers_pool["bottoms"].append("win/7x7/ets")
-      layers_pool["bottoms"].append("win/9x9/ets")
 
     layers_pool["tops"] = []
     #layers_pool["tops"].append("pool/5x5/ets")
-    layers_pool["tops"].append("pool/5x5/rf")
+    layers_pool["tops"].append("pool/3x3/rf")
     #layers_pool["tops"].append("pool/7x7/ets")
-    layers_pool["tops"].append("pool/7x7/rf")
+    layers_pool["tops"].append("pool/4x4/rf")
     #layers_pool["tops"].append("pool/9x9/ets")
-    layers_pool["tops"].append("pool/9x9/rf")
+    layers_pool["tops"].append("pool/5x5/rf")
 
     if tree_diversity_mgs:
+      layers_pool["tops"].append("pool/3x3/ets")
+      layers_pool["tops"].append("pool/4x4/ets")
       layers_pool["tops"].append("pool/5x5/ets")
-      layers_pool["tops"].append("pool/7x7/ets")
-      layers_pool["tops"].append("pool/9x9/ets")
 
     layers_pool["pool_method"] = "avg"
     layers_pool["win_x"] = 2
     layers_pool["win_y"] = 2
 
     layers = []
+    layers.append(layer_3x3)
+    layers.append(layer_4x4)
     layers.append(layer_5x5)
-    layers.append(layer_7x7)
-    layers.append(layer_9x9)
     layers.append(layers_pool)
     net["layers"] = layers
 
