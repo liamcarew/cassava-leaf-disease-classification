c6d6d6ba0fe7cea49fe4de722ef647ccf106b778
liamcarew <liam.carew1@gmail.com>
2022-09-30 11:27:51 +0200

    Updates to model comb scripts.

--- model_comb_scripts/model_comb_11.py
+++ model_comb_scripts/model_comb_11.py
@@ -72,9 +72,9 @@ deep_learning_gridsearch(
   validation_data = validation_data,
   x_val = x_val,
   y_val = y_val,
-  num_epochs = 150,
+  num_epochs = 100,
   random_state = 1,
   start_fine_tune_layer_name = 'pool2_conv',
-  es_patience = 100,
+  es_patience = 75,
   gpu_devices = gpu_devices)
 
